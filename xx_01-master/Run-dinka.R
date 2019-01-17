627d7973bfed49b6a0c4eb2e4f2f7a90e1347b91
Dinka Smajlagic <dsm001@med0221043.klientdrift.uib.no>
2019-01-17 11:30:20 +0100

    Added a function in code file and created new Run-dinka.R script

--- xx_01-master/Run-dinka.R
+++ xx_01-master/Run-dinka.R
@@ -13,6 +13,8 @@ setwd("/git/xx_01_submission/xx_01-master")
 fileSources = file.path("code", list.files("code", pattern = "*.[rR]$"))
 sapply(fileSources, source, .GlobalEnv)
 
+source("code/FillNAgaps.R")
+
 #Load the data
 d <- readRDS("data_raw/individual_level_data.RDS")
 #Create new numerical value
@@ -24,38 +26,7 @@ X <- split(d,d$location)
 #Create fake data
 fakedata <- data.table(date = seq.Date(as.Date("2000-01-01"), as.Date("2010-12-31"), by = 1))
 
-#Replace NA with the first good value
-fillNAgaps <- function(x, firstBack=FALSE) {
-  ## NA's in a vector or factor are replaced with last non-NA values
-  ## If firstBack is TRUE, it will fill in leading NA's with the first
-  ## non-NA value. If FALSE, it will not change leading NA's.
-  # If it's a factor, store the level labels and convert to integer
-  lvls <- NULL
-  if (is.factor(x)) {
-    lvls <- levels(x)
-    x    <- as.integer(x)
-  }
-  
-  goodIdx <- !is.na(x)
-  
-  # These are the non-NA values from x only
-  # Add a leading NA or take the first good value, depending on firstBack   
-  if (firstBack)   goodVals <- c(x[goodIdx][1], x[goodIdx])
-  else             goodVals <- c(NA,            x[goodIdx])
-  
-  # Fill the indices of the output vector with the indices pulled from
-  # these offsets of goodVals. Add 1 to avoid indexing to zero.
-  fillIdx <- cumsum(goodIdx)+1
-  
-  x <- goodVals[fillIdx]
-  
-  # If it was originally a factor, convert it back
-  if (!is.null(lvls)) {
-    x <- factor(x, levels=seq_along(lvls), labels=lvls)
-  }
-  
-  x
-}
+
 
 for (i in 1:422) {
      #Aggregate by the date for every municipality + don't forget rows/days with zero sick people
